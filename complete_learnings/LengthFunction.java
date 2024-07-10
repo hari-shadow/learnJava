0b81d564d25bca5e63b2f20d5b1bc4e7ed94d6d9
hari-shadow <hariharanlaligam@gmail.com>
2024-07-10 19:12:32 +0530

    tried to recreate the length function in java

---
+++ complete_learnings/LengthFunction.java
@@ -0,0 +1,10 @@
+public class LengthFunction {
+    public static void main(String[] args) {
+        int count = 0;
+        int[] arr = {1, 3, 5, 45, 6566, 4734, 63456};
+        for (int a : arr) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
