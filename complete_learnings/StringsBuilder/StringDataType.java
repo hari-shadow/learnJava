59bfc0f122616e9850fe489bbd3f5f30381676d3
hari-shadow <hariharanlaligam@gmail.com>
2024-07-25 19:50:12 +0530

    learnt String data types

---
+++ complete_learnings/StringsBuilder/StringDataType.java
@@ -0,0 +1,19 @@
+package StringsBuilder;
+
+import java.util.ArrayList;
+
+public class StringDataType {
+    public static void main(String[] args) {
+//        String a = " hari ";
+//        String b = " hari ";
+//        System.out.println(a);
+//
+//        System.out.println(b);
+//        float a = 25.4651f;
+//        System.out.printf("df %.2f",a);
+        ArrayList<Integer> just = new ArrayList<>();
+        just.add(5);
+        System.out.println("sdf" + just + 5);
+        System.out.println(new ArrayList<>() +""+ new int[3]);
+    }
+}
